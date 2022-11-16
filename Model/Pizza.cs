b36fe3f6c9f3799e80613f58a70ac45fce764b63
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-11-16 16:47:54 -0500

    Updated the size slizders.

--- Model/Pizza.cs
+++ Model/Pizza.cs
@@ -8,9 +8,10 @@ namespace BlazingPizza
     /// </summary>
     public class Pizza
     {
-        public const int DefaultSize = 12;
+
         public const int MinimumSize = 9;
         public const int MaximumSize = 17;
+        public int DefaultSize { get; set; } = 12;        
 
         public int Id { get; set; }
 
